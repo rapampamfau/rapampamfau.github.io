b60beb75827e3686896137ca23d9828e926259f7
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-05-17 17:52:39 +0200

    Updated frontend

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://kodilla-tasks-rs.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const trelloApiRoot = 'https://kodilla-tasks-rs.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
